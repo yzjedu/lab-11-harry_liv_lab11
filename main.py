af5c50b0ed5fbd7ece4743803d585ad868180706
HarryLi2028 <Hli2@loyola.edu>
2024-11-30 17:04:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+# Programmers: Harry Li, Liv Oakes
+# Course:  CS151*06
+# Due Date: 11/20/2024
+# Lab Assignment: Lab 11
+# Problem Statement: 
+# Data In: 
+# Data Out: 
+
 def check_file(file_name):
     try:
         with open(file_name, 'r') as file:
