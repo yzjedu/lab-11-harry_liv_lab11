7863c6eadcde77d6f0db5158a777b1dec1807330
HarryLi2028 <Hli2@loyola.edu>
2024-11-30 17:03:11 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+def check_file(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            morse_file = []
+            for line in file:
+                stripped_line = line.strip()
+                if stripped_line:
+                    morse_file.append(stripped_line)
+        return morse_file
+    except FileNotFoundError:  
+        print(f'Error: File {file_name} not found.')
+        return []
+def morse_key(file_name):
+    key = []
+    with open(file_name, 'r') as file:
+
+def morse_code_translation():
