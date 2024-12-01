08a926a5696431869b661f956a06d11c1559d028
HarryLi2028 <Hli2@loyola.edu>
2024-11-30 21:53:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,20 +6,37 @@
 # Data In: 
 # Data Out: 
 
-def check_file(file_name):
-    try:
-        with open(file_name, 'r') as file:
-            morse_file = []
-            for line in file:
-                stripped_line = line.strip()
-                if stripped_line:
-                    morse_file.append(stripped_line)
-        return morse_file
-    except FileNotFoundError:  
-        print(f'Error: File {file_name} not found.')
-        return []
-def morse_key(file_name):
-    key = []
-    with open(file_name, 'r') as file:
+import os 
 
+def check_file(file_name):
+    #See if file exists and store in a table
+    if os.path.isfile(file_name):
+        exist = 0
+    else:
+        exist = 1
+    return exist 
+    
+def morse_key(file_name):
+    #Create a dictionary to store the corresponding translations
+    key_dictionary = {}
+    #Open the file for reading, and for each line store the key and letter or charater to the dictionary
+    with open(file_name, 'r') as file:
+        for line in file:
+            key = line.strip().split('\t')
+            key_dictionary[key[1]] = key[0]
+    #Return the dictionary 
+    return key_dictionary
+    
 def morse_code_translation():
+    
+
+def main():
+    
+    morse_dictionary = morse_key(morsecode.txt)
+    
+    translate_file = input('Enter the file name you want to translate: ')
+    exist = check_file(translate_file)
+    while exist != 0:
+        translate_file = input('Please enter a valid file name you want to translate: ')
+        exist = check_file(translate_file)
+        
